e58463481afdc4865adefeff777a071c3d599a82
mstroeck <michael@stroeck.com>
2007-02-05 17:52:23 +0000

    Big commit: We're polished metal now. Just need to add the new SplitViews.

---
+++ AMPolishedButton.h
@@ -0,0 +1,16 @@
+//
+//  AMPolishedButton.h
+//  Polished Button
+//
+//  Created by Andy Matuschak on 7/31/06.
+//  Copyright 2006 Andy Matuschak. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AMPolishedButton : NSButton {
+
+}
+
+@end
