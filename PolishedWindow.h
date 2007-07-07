36cb34ff4fc4261040122eccee93d8bc1c0bc967
stevewpalmer <USER@DOMAIN>
2007-07-07 09:03:03 +0000

    Cache the background images. This fixes the black toolbar bug for now but it isn't what I'd consider to be a real fix as I haven't been able to identify where the image is being corrupted.
    The problem was appearing with setting bgColor from the top_middle.png. This
    is normally a light grey colour but after a few iterations, it becomes black.
    I think we're hitting some resource issue.
    
    Also, with this fix, I'm still seeing a painting issue. After a little while,
    the top middle row ended up being painted with a while sequence of spinner
    controls along the top.

--- PolishedWindow.h
+++ PolishedWindow.h
@@ -1,16 +1,23 @@
 //
-//  PolishedWindow.h
-//  TunesWindow
+//	PolishedWindow.h
+//	TunesWindow
 //
-//  Created by Matt Gemmell on 12/02/2006.
-//  Copyright 2006 Magic Aubergine. All rights reserved.
+//	Created by Matt Gemmell on 12/02/2006.
+//	Copyright 2006 Magic Aubergine. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 #import "SquareWindow.h"
 
 @interface PolishedWindow : SquareWindow {
-    BOOL forceDisplay;
+	BOOL forceDisplay;
+
+	NSImage *bottomMiddle;
+	NSImage *topLeft;
+	NSImage *topMiddle;
+	NSImage *topRight;
+	
+	NSColor *bgColor;
 }
 
 // Public functions
