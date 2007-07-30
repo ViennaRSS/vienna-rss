c22c7ffbba5542e3772e0173edcac5c84c7ba678
johnson1234 <USER@DOMAIN>
2007-07-30 01:49:32 +0000

    Add script to build beta version of Vienna.

--- scripts/release_vienna.sh
+++ scripts/release_vienna.sh
@@ -5,5 +5,5 @@ cd ..
 echo "Changed to project directory"
 
 echo "Building Vienna"
-xcodebuild -project Vienna.xcodeproj -target Vienna -configuration Deployment clean build DEPLOYMENT_POSTPROCESSING=YES
+xcodebuild -project Vienna.xcodeproj -target Vienna -configuration Deployment clean build DEPLOYMENT_POSTPROCESSING=YES VIENNA_IS_BETA=NO
 echo "Built Vienna"
