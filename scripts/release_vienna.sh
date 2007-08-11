6011bc3a33c842f0541c366e99305e316558780a
johnson1234 <USER@DOMAIN>
2007-08-11 14:35:16 +0000

    Update release scripts to create upload version of Vienna with full symbols for debugging crash reports.

--- scripts/release_vienna.sh
+++ scripts/release_vienna.sh
@@ -4,6 +4,10 @@ echo "Changing to project directory"
 cd ..
 echo "Changed to project directory"
 
-echo "Building Vienna"
+echo "Building Vienna configuration DeploymentWithSymbols"
+xcodebuild -project Vienna.xcodeproj -target Vienna -configuration DeploymentWithSymbols clean build
+echo "Built Vienna configuration DeploymentWithSymbols"
+
+echo "Building Vienna configuration Deployment"
 xcodebuild -project Vienna.xcodeproj -target Vienna -configuration Deployment clean build DEPLOYMENT_POSTPROCESSING=YES VIENNA_IS_BETA=NO
-echo "Built Vienna"
+echo "Built Vienna configuration Deployment"
