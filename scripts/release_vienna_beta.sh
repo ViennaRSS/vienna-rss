6d5d0c6007c2f5353d33d0c4b5986b933fd8fd84
johnson1234 <USER@DOMAIN>
2008-05-25 17:50:48 +0000

    Change debugging format from Stabs to Dwarf. TODO: manually strip the executable? Apparently Xcode runs the strip last, after we've already zipped the files for upload.

--- scripts/release_vienna_beta.sh
+++ scripts/release_vienna_beta.sh
@@ -4,10 +4,6 @@ echo "Changing to project directory"
 cd ..
 echo "Changed to project directory"
 
-echo "Building Vienna configuration DeploymentWithSymbols"
-xcodebuild -project Vienna.xcodeproj -target Vienna -configuration DeploymentWithSymbols clean build VIENNA_CHANGELOG_SUFFIX="_beta"
-echo "Built Vienna configuration DeploymentWithSymbols"
-
 echo "Building Vienna configuration Deployment"
 xcodebuild -project Vienna.xcodeproj -target Vienna -configuration Deployment clean build DEPLOYMENT_POSTPROCESSING=YES VIENNA_CHANGELOG_SUFFIX="_beta"
 echo "Built Vienna configuration Deployment"
