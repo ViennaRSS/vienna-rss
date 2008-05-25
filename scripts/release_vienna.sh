08d4e54ac7d2f0dbc0cb7c8b880bf32b13ac2299
johnson1234 <USER@DOMAIN>
2008-05-25 18:37:52 +0000

    Add a Release target. This gets around the strip linked product problem, and it also allows us to build for release in Xcode. Perhaps we should also add a ReleaseBeta target.

--- scripts/release_vienna.sh
+++ scripts/release_vienna.sh
@@ -5,5 +5,5 @@ cd ..
 echo "Changed to project directory"
 
 echo "Building Vienna configuration Deployment"
-xcodebuild -project Vienna.xcodeproj -target Vienna -configuration Deployment clean build DEPLOYMENT_POSTPROCESSING=YES
+xcodebuild -project Vienna.xcodeproj -target Release -configuration Deployment clean build
 echo "Built Vienna configuration Deployment"
--- scripts/release_vienna_beta.sh
+++ scripts/release_vienna_beta.sh
@@ -5,5 +5,5 @@ cd ..
 echo "Changed to project directory"
 
 echo "Building Vienna configuration Deployment"
-xcodebuild -project Vienna.xcodeproj -target Vienna -configuration Deployment clean build DEPLOYMENT_POSTPROCESSING=YES VIENNA_CHANGELOG_SUFFIX="_beta"
+xcodebuild -project Vienna.xcodeproj -target Release -configuration Deployment clean build VIENNA_CHANGELOG_SUFFIX="_beta"
 echo "Built Vienna configuration Deployment"
