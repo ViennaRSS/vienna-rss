dcd818ba1ce3774b9fa189febdfbb7f30327714d
mstroeck <michael@stroeck.com>
2009-10-22 18:11:04 +0000

    Finish error handling code.

--- 2.4.0/BrowserPane.m
+++ 2.4.0/BrowserPane.m
@@ -376,6 +376,20 @@
 			[self setError:error];
 		[self endFrameLoad];
 	}
+	
+	// Use a warning sign as favicon
+	[iconImage setImage:[NSImage imageNamed:@"folderError.tiff"]];
+	
+	// Load the localized verion of the error page
+	NSString * pathToErrorPage = [[NSBundle bundleForClass:[self class]] pathForResource:@"errorpage" ofType:@"html"];
+	if (pathToErrorPage != nil)
+	{
+		NSString *errorMessage = [NSString stringWithContentsOfFile:pathToErrorPage encoding:NSUTF8StringEncoding error:NULL];
+		if (errorMessage != nil)
+		{
+			[frame loadAlternateHTMLString:errorMessage baseURL:[NSURL fileURLWithPath:pathToErrorPage isDirectory:NO] forUnreachableURL:[[[frame dataSource] request] URL]];
+		}
+	}		
 }
 
 /* didFinishLoadForFrame
