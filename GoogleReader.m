33fc07a220cce6a3937ba4d4dcc9841a193ccc63
Barijaona Ramaholimihaso <github.com@barijaona.com>
2012-11-04 07:23:17 +0300

    Fix some leaks and some Xcode analyze warnings.

--- GoogleReader.m
+++ GoogleReader.m
@@ -582,7 +582,7 @@ JSONDecoder * jsonDecoder;
 				folderName = [folderNames lastObject];
 				// NNW nested folder char: — 
 				
-				NSMutableArray * params = [NSMutableArray arrayWithObjects:[folderNames mutableCopy], [NSNumber numberWithInt:MA_Root_Folder], nil];                
+				NSMutableArray * params = [NSMutableArray arrayWithObjects:[[folderNames mutableCopy] autorelease], [NSNumber numberWithInt:MA_Root_Folder], nil];
 				[self performSelectorOnMainThread:@selector(createFolders:) withObject:params waitUntilDone:YES];
 				break; //In case of multiple labels, we retain only the first one
 			} 
