fce2c0ce0ea2478038dcecfde16c48ef3eb13d5d
Barijaona Ramaholimihaso <github.com@barijaona.com>
2012-12-31 21:25:20 +0300

    Snow Leopard is less lenient than Mountain Lion for interpreting date formats.

--- src/AppController.m
+++ src/AppController.m
@@ -128,7 +128,7 @@ static void MySleepCallBack(void * x, io_service_t y, natural_t messageType, voi
 
 // C array of NSDateFormatter's : creating a NSDateFormatter is very expensive, so we create
 //  those we need early in the program launch and keep them in memory.
-#define kNumberOfDateFormatters 4
+#define kNumberOfDateFormatters 6
 static NSDateFormatter * dateFormatterArray[kNumberOfDateFormatters];
 
 static NSLock * dateFormatters_lock;
@@ -1008,12 +1008,15 @@ static void MyScriptsFolderWatcherCallBack(FNMessage message, OptionBits flags, 
 
 	//For the different date formats, see <http://unicode.org/reports/tr35/#Date_Format_Patterns>
 	// 2010-09-28T15:31:25Z and 2010-09-28T17:31:25+02:00
-	[dateFormatterArray[0] setDateFormat:@"yyyy-MM-dd'T'HH:mm:sszzz"];
+	[dateFormatterArray[0] setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZZZ"];
 	// 2010-09-28T15:31:25.815+02:00
-	[dateFormatterArray[2] setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSSzzz"];
+	[dateFormatterArray[1] setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSSZZZ"];
 	// "Sat, 13 Dec 2008 18:45:15 EAT" and "Fri, 12 Dec 2008 18:45:15 -08:00"
-	[dateFormatterArray[1] setDateFormat:@"EEE, dd MMM yyyy HH:mm:ss zzz"];
-	[dateFormatterArray[3] setDateFormat:@"EEE, dd MMM yyyy HH:mm:ss"];
+	[dateFormatterArray[2] setDateFormat:@"EEE, dd MMM yyyy HH:mm:ss ZZZ"];
+	[dateFormatterArray[3] setDateFormat:@"EEE, dd MMM yyyy HH:mm:ss zzz"];
+	[dateFormatterArray[4] setDateFormat:@"EEE, dd MMM yyyy HH:mm:ss"];
+	// Other exotic date formats
+	[dateFormatterArray[5] setDateFormat:@"yyyy-MM-dd HH:mm:ss ZZZ"];
 
 	[enUS release];
 	// end of initialization of date formatters
