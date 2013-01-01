da08c7d24fb325c971dd300db0d586feb1620e2a
dak180 <dak180@users.sourceforge.net>
2013-01-01 15:08:28 -0500

    Use precompiled code sign requirements to keep compatibility with 10.5+.
    Note: code sign requirements must be compiled on 10.5 to be compatible with it.

--- configs/scripts/Make-ID-Template.sh
+++ configs/scripts/Make-ID-Template.sh
@@ -5,6 +5,9 @@ if [ ! -f configs/CS-ID.xcconfig ]; then
 // Global settings for Code Signing
 
 CODE_SIGN_IDENTITY = 
+PRIVATE_KEY_PATH = 
+
+CODE_SIGN_REQUIREMENTS_PATH = "$(SRCROOT)/Resources/codesignrequirement.csreq"
 CODE_SIGN_RESOURCE_RULES_PATH = "$(SRCROOT)/Resources/ResourceRules.plist"
 
 EOF
