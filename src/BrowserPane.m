ec913b32d64b7448d5593aeb6d31c65417e25a5e
Barijaona Ramaholimihaso <github.com@barijaona.com>
2013-06-16 16:53:27 +0300

    Fix issue #190 : add a file chooser.

--- src/BrowserPane.m
+++ src/BrowserPane.m
@@ -517,6 +517,24 @@
 	return NSAlertDefaultReturn == result;
 }
 
+- (void)webView:(WebView *)sender runOpenPanelForFileButtonWithResultListener:(id < WebOpenPanelResultListener >)resultListener
+{
+	// Create the File Open Dialog class.
+	NSOpenPanel* openDlg = [NSOpenPanel openPanel];
+
+	// Enable the selection of files in the dialog.
+	[openDlg setCanChooseFiles:YES];
+
+	// Enable the selection of directories in the dialog.
+	[openDlg setCanChooseDirectories:NO];
+
+	if ( [openDlg runModal] == NSOKButton )
+	{
+		NSArray* files = [[openDlg URLs]valueForKey:@"relativePath"];
+		[resultListener chooseFilenames:files];
+	}
+}
+
 /* setFrame
  * Trap this to stop scripts from resizing the main Vienna window.
  */
