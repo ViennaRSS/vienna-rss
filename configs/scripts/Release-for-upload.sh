8f8517c81b99ac4ae7eb0f463c01184b172dc845
dak180 <dak180@users.sourceforge.net>
2014-08-15 21:58:17 -0400

    Check for a dirty working directory correctly.
    Revert "Apply a strict checking of the version control tag"
    
    This reverts commit 9482c4fc631b8252f9b10ff2a7335c8395b434b0.

--- configs/scripts/Release-for-upload.sh
+++ configs/scripts/Release-for-upload.sh
@@ -92,8 +92,13 @@ if [ ! "${CONFIGURATION}" = "Deployment" ]; then
 	echo "error: This should only be run as Deployment" >&2
 	exit 1
 fi
+# Fail if not clean
+if [ ! "${VCS_WC_MODIFIED}" == "0" ]; then
+	echo 'error: The Working directory is not clean; please commit or revert any changes.' 1>&2
+	exit 1
+fi
 # Fail if incorrectly tagged
-if ! git describe --dirty --exact-match --match "${VCS_TAG}"; then
+if [[ "${VCS_TICK}" == "0" ]] && ! git describe --exact-match "${VCS_TAG}"; then
 	echo 'error: The tag is not annotated; please redo the tag with `git tag -s` or `git tag -a`.' 1>&2
 	exit 1
 fi
