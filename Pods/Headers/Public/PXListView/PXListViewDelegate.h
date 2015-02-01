8435023ac1719092fd836b85876ae5b076844b9d
Barijaona Ramaholimihaso <github.com@barijaona.com>
2015-02-01 22:47:04 +0300

    Pod install
    Note : we use Cocoapods 0.36.0.beta.2 (which allows setting architecture)

---
+++ Pods/Headers/Public/PXListView/PXListViewDelegate.h
@@ -0,0 +1,1 @@
+../../../PXListView/Classes/PXListViewDelegate.h
---
+++ Pods/PXListView/Classes/PXListView+Private.h
@@ -0,0 +1,43 @@
+//
+//  PXListView+Private.h
+//  PXListView
+//
+//  Created by Alex Rozanski on 01/06/2010.
+//  Copyright 2010 Alex Rozanski. http://perspx.com. All rights reserved.
+//
+
+// This is a renamed copy of UKIsDragStart from <http://github.com/uliwitness/UliKit>:
+// Possible return values from UKIsDragStart:
+enum
+{
+	PXIsDragStartMouseReleased = 0,
+	PXIsDragStartTimedOut,
+	PXIsDragStartMouseMovedHorizontally,
+	PXIsDragStartMouseMovedVertically
+};
+typedef NSInteger PXIsDragStartResult;
+
+
+@interface PXListView ()
+
+- (NSRect)contentViewRect;
+
+- (void)cacheCellLayout;
+- (void)layoutCells;
+- (void)layoutCell:(PXListViewCell*)cell atRow:(NSUInteger)row;
+
+- (void)addCellsFromExtendedRange;
+- (PXListViewCell*)visibleCellForRow:(NSUInteger)row;
+- (NSArray*)visibleCellsForRowIndexes:(NSIndexSet*)rows;
+
+- (NSUInteger)numberOfRows;
+- (void)deselectRowIndexes:(NSIndexSet*)rows;
+- (void)postSelectionDidChangeNotification;
+
+- (void)updateCells;
+- (void)enqueueCell:(PXListViewCell*)cell;
+
+- (void)contentViewBoundsDidChange:(NSNotification*)notification;
+-(void)layoutCellsForResizeEvent;
+
+@end
