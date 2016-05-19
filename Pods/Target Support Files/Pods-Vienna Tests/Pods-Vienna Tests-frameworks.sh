95bec290a4bca00873b4f5bffb2e6d83999cca0b
György Tóth <tothgy@mail.ru>
2016-05-19 19:29:36 +0200

    Add files to test target

--- Pods/Target Support Files/Pods-Vienna Tests/Pods-Vienna Tests-frameworks.sh
+++ Pods/Target Support Files/Pods-Vienna Tests/Pods-Vienna Tests-frameworks.sh
@@ -82,3 +82,13 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Development" ]]; then
+  install_framework "${PODS_ROOT}/Sparkle/Sparkle.framework"
+fi
+if [[ "$CONFIGURATION" == "Deployment" ]]; then
+  install_framework "${PODS_ROOT}/Sparkle/Sparkle.framework"
+fi
+if [[ "$CONFIGURATION" == "StaticAnalyzer" ]]; then
+  install_framework "${PODS_ROOT}/Sparkle/Sparkle.framework"
+fi
