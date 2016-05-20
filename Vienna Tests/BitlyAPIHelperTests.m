6b494792ed833cd59ed7cafb96f6e5748a1ad7cb
György Tóth <tothgy@mail.ru>
2016-05-20 19:22:49 +0200

    Move performance tests to Performance tests target

--- Vienna Tests/BitlyAPIHelperTests.m
+++ Vienna Tests/BitlyAPIHelperTests.m
@@ -34,11 +34,4 @@
     XCTAssertTrue([shortURL containsString:@"bit.ly"]);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
 @end
