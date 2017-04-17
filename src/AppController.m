efced6bf71eeef9d54bea3a1a600a5526c044698
Barijaona Ramaholimihaso <github.com@barijaona.com>
2017-04-17 18:08:55 +0300

    Fix wake after sleep issue
    The 15 seconds delay after sleep should apply to OpenReader too.

--- src/AppController.m
+++ src/AppController.m
@@ -286,9 +286,6 @@ static void MySleepCallBack(void * refCon, io_service_t service, natural_t messa
                     NSTimeInterval interval = -lastRefresh.timeIntervalSinceNow;
                     if (interval > frequency)
                     {
-                        if ([Preferences standardPreferences].syncGoogleReader) {
-                            [[GoogleReader sharedManager] getTokenForRequest:nil];
-						}
                         [NSTimer scheduledTimerWithTimeInterval:15.0
                                                          target:app
                                                        selector:@selector(refreshOnTimer:)
