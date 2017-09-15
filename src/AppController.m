ViennaRSS/vienna-rss

commit 2fbf5e9ee93855cfb1420e19ff4002581fde9521

Changed sequence for skipping folders
We select the next folder before marking current read.

src/AppController.m
@@ -3075,8 +3075,11 @@ withReplyEvent:(NSAppleEventDescriptor *)replyEvent
 {
 	if (!db.readOnly)
 	{
-		[articleController markAllReadByArray:foldersTree.selectedFolders withUndo:YES withRefresh:YES];
-		[self viewNextUnread:self];
+		NSArray * currentSelectedFolders = foldersTree.selectedFolders;
+		NSInteger nextFolderInTree = [foldersTree nextFolderWithUnread:articleController.currentFolderId];
+		[foldersTree selectFolder:nextFolderInTree];
+        [articleController ensureSelectedArticle:NO];
+		[articleController markAllReadByArray:currentSelectedFolders withUndo:YES withRefresh:YES];
 	}
 }
 

commit 6a3022caaae405a373d5353b0c3f56c101b1755d

Fix Skip Folder behavior. Skip Folder previously selected the first unread article in the next folder with unread.
This bug was introduced in 2fbf5e9ee93855cfb1420e19ff4002581fde9521

src/AppController.m
@@ -3067,9 +3067,10 @@ withReplyEvent:(NSAppleEventDescriptor *)replyEvent
 	if (!db.readOnly)
 	{
 		[articleController markAllFoldersReadByArray:foldersTree.selectedFolders];
-		NSInteger nextFolderInTree = [foldersTree nextFolderWithUnread:articleController.currentFolderId];
-		[foldersTree selectFolder:nextFolderInTree];
-        [articleController ensureSelectedArticle];
+		if (db.countOfUnread > 0)
+		{
+			[articleController displayNextFolderWithUnread];
+		}
 	}
 }
 

src/ArticleController.h
@@ -78,6 +78,7 @@
 @property (nonatomic, readonly, copy) NSArray *allArticles;
 -(void)displayFirstUnread;
 -(void)displayNextUnread;
+-(void)displayNextFolderWithUnread;
 @property (nonatomic, readonly, copy) NSString *searchPlaceholderString;
 -(void)reloadArrayOfArticles;
 -(void)displayFolder:(NSInteger)newFolderId;

src/ArticleController.m
@@ -373,45 +373,48 @@
  */
 -(void)displayNextUnread
 {
-	// remember current article
+	// mark current article read
 	Article * currentArticle = self.selectedArticle;
-	Folder * currentFolder = [[Database sharedManager] folderFromID:currentFolderId];
-
-	BOOL currentFolderExhausted = NO;
-	// Search other articles in the same folder, starting from current position
-	if (![mainArticleView viewNextUnreadInFolder])
-	{
-		// If nothing found and smart folder, search if we have other fresh articles from same folder
-		if (IsSmartFolder(currentFolder) || IsTrashFolder(currentFolder) || IsSearchFolder(currentFolder))
-		{
-			if (![mainArticleView selectFirstUnreadInFolder] || self.selectedArticle == currentArticle)
-			{
-				currentFolderExhausted = YES;
-			}
-		}
-		else
-		{
-			currentFolderExhausted = YES;
-		}
-	}
-
-	if (currentFolderExhausted  && ([[Database sharedManager] countOfUnread] > 1 || currentArticle == nil || currentArticle.read) )
-	{
-		// try other folders
-		NSInteger nextFolderWithUnread = [foldersTree nextFolderWithUnread:currentFolderId];
-		if (nextFolderWithUnread != -1)
-		{
-			// Seed in order to select the first unread article.
-			firstUnreadArticleRequired = YES;
-			// Select the folder
-			[foldersTree selectFolder:nextFolderWithUnread];
-		}
-	}
-
-	// mark read previously selected article
 	if (currentArticle != nil && !currentArticle.read)
 	{
 		[self markReadByArray:@[currentArticle] readFlag:YES];
+	}
+	
+	// If there are any unread articles then select the nexst one
+	if ([Database sharedManager].countOfUnread > 0)
+	{
+		// Search other articles in the same folder, starting from current position
+		if (![mainArticleView viewNextUnreadInFolder])
+		{
+			// If nothing found and smart folder, search if we have other fresh articles from same folder
+			Folder * currentFolder = [[Database sharedManager] folderFromID:currentFolderId];
+			if (IsSmartFolder(currentFolder) || IsTrashFolder(currentFolder) || IsSearchFolder(currentFolder))
+			{
+				if (![mainArticleView selectFirstUnreadInFolder])
+				{
+					[self displayNextFolderWithUnread];
+				}
+			}
+			else
+			{
+				[self displayNextFolderWithUnread];
+			}
+		}
+	}
+}
+
+/* displayNextFolderWithUnread
+ * Instructs the current article view to display the next folder with unread articles
+ * in the database.
+ */
+-(void)displayNextFolderWithUnread
+{
+	NSInteger nextFolderWithUnread = [foldersTree nextFolderWithUnread:currentFolderId];
+	if (nextFolderWithUnread != -1)
+	{
+		// Seed in order to select the first unread article.
+		firstUnreadArticleRequired = YES;
+		[foldersTree selectFolder:nextFolderWithUnread];
 	}
 }
 