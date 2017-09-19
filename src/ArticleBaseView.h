ViennaRSS/vienna-rss

commit 87a2c7cb080b53d6a884d616029fe21650e040dd

Deselect articles before deleting them. This ensures that the article pane will reload.
Fix for https://forums.cocoaforge.com/viewtopic.php?f=18&t=27004
This bug was introduced in 331564bc4dc6d1768c09f10b3dcb41b10a13654b

src/ArticleBaseView.h
@@ -29,7 +29,8 @@
 	@property (nonatomic, readonly, strong) Article *selectedArticle;
 	@property (nonatomic, readonly, copy) NSArray *markedArticleRange;
 	-(void)saveTableSettings;
-	-(void)ensureSelectedArticle:(BOOL)singleSelection;
+	-(void)selectPreviousArticle;
+	-(void)ensureSelectedArticle;
 	-(void)startLoadIndicator;
 	-(void)stopLoadIndicator;
 @end

src/ArticleController.m
@@ -148,7 +148,7 @@
 	if (currentSelectedArticle != nil)
 	{
 	    [self selectFolderAndArticle:currentFolderId guid:currentSelectedArticle.guid];
-	    [self ensureSelectedArticle:NO];
+	    [self ensureSelectedArticle];
 		[[NSNotificationCenter defaultCenter] postNotificationName:@"MA_Notify_ArticleViewChange" object:nil];
 	}
 
@@ -223,10 +223,10 @@
  * Ensures that an article is selected in the list and that any selected
  * article is scrolled into view.
  */
--(void)ensureSelectedArticle:(BOOL)singleSelection
+-(void)ensureSelectedArticle
 {
 	if (reloadArrayOfArticlesSemaphor <= 0) {
-	    [mainArticleView ensureSelectedArticle:singleSelection];
+	    [mainArticleView ensureSelectedArticle];
 	} else {
 	    requireSelectArticleAfterReload = YES;
 	}
@@ -499,7 +499,7 @@
 			
             if (requireSelectArticleAfterReload)
             {
-                [self ensureSelectedArticle:NO];
+                [self ensureSelectedArticle];
 				requireSelectArticleAfterReload = NO;
             }
 
@@ -613,6 +613,7 @@
 	if (deleteFlag) {
 	    [self innerMarkReadByRefsArray:articleArray readFlag:YES];
         [self innerMarkFlaggedByArray:articleArray flagged:NO];
+		[mainArticleView selectPreviousArticle];
 	}
 
 	// Iterate over every selected article in the table and set the deleted
@@ -644,7 +645,7 @@
 	{
 		[mainArticleView refreshFolder:MA_Refresh_RedrawList];
 		if (currentArrayOfArticles.count > 0u)
-			[mainArticleView ensureSelectedArticle:YES];
+			[mainArticleView ensureSelectedArticle];
 		else
 			[NSApp.mainWindow makeFirstResponder:foldersTree.mainView];
 	}
@@ -660,6 +661,7 @@
 	NSMutableArray * folderArrayCopy = [NSMutableArray arrayWithArray:folderArrayOfArticles];
 	
 	[self innerMarkReadByRefsArray:articleArray readFlag:YES];
+	[mainArticleView selectPreviousArticle];
 
 	// Iterate over every selected article in the table and remove it from
 	// the database.
@@ -677,7 +679,7 @@
 
 	// Ensure there's a valid selection
     if (currentArrayOfArticles.count > 0u) {
-		[mainArticleView ensureSelectedArticle:YES];
+		[mainArticleView ensureSelectedArticle];
     } else {
 		[NSApp.mainWindow makeFirstResponder:foldersTree.mainView];
     }

src/ArticleListView.m
@@ -508,27 +508,31 @@ static const CGFloat MA_Minimum_Article_Pane_Dimension = 80;
 	}
 }
 
-/* ensureSelectedArticle
- * Ensure that there is a selected article and that it is visible.
+/* selectPreviousArticle
+ * Select the previous article in the list if possible.
  */
--(void)ensureSelectedArticle:(BOOL)singleSelection
+-(void)selectPreviousArticle
 {
-	if (singleSelection)
+	NSUInteger nextRow = articleList.selectedRowIndexes.firstIndex;
+	if (nextRow != NSNotFound && nextRow > 0)
 	{
-		NSUInteger nextRow = articleList.selectedRowIndexes.firstIndex;
-		NSUInteger articlesCount = articleController.allArticles.count;
-
-		if (nextRow >= articlesCount)
-			nextRow = articlesCount - 1;
-		[self makeRowSelectedAndVisible:nextRow];
+		[self makeRowSelectedAndVisible:--nextRow];
 	}
 	else
 	{
-		if (articleList.selectedRow == -1)
-			[self makeRowSelectedAndVisible:0];
-		else
-			[articleList scrollRowToVisible:articleList.selectedRow];
+		[articleList deselectAll:self];
 	}
+}
+
+/* ensureSelectedArticle
+ * Ensure that there is a selected article and that it is visible.
+ */
+-(void)ensureSelectedArticle
+{
+	if (articleList.selectedRow == -1)
+		[self makeRowSelectedAndVisible:0];
+	else
+		[articleList scrollRowToVisible:articleList.selectedRow];
 }
 
 /* updateVisibleColumns

src/UnifiedDisplayView.m
@@ -379,27 +379,31 @@
 	}
 }
 
-/* ensureSelectedArticle
- * Ensure that there is a selected article and that it is visible.
+/* selectPreviousArticle
+ * Select the previous article in the list if possible.
  */
--(void)ensureSelectedArticle:(BOOL)singleSelection
+-(void)selectPreviousArticle
 {
-	if (singleSelection)
+	NSUInteger nextRow = articleList.selectedRowIndexes.firstIndex;
+	if (nextRow != NSNotFound && nextRow > 0)
 	{
-		NSUInteger nextRow =articleList.selectedRowIndexes.firstIndex;
-		NSUInteger articlesCount = articleController.allArticles.count;
-
-		if (nextRow >= articlesCount)
-			nextRow = articlesCount - 1;
-		[self makeRowSelectedAndVisible:nextRow];
+		[self makeRowSelectedAndVisible:--nextRow];
 	}
 	else
 	{
-		if (articleList.selectedRow == -1)
-			[self makeRowSelectedAndVisible:0];
-		else
-			[articleList scrollRowToVisible:articleList.selectedRow];
+		[articleList deselectAll:self];
 	}
+}
+
+/* ensureSelectedArticle
+ * Ensure that there is a selected article and that it is visible.
+ */
+-(void)ensureSelectedArticle
+{
+	if (articleList.selectedRow == -1)
+		[self makeRowSelectedAndVisible:0];
+	else
+		[articleList scrollRowToVisible:articleList.selectedRow];
 }
 
 /* scrollToArticle

src/AppController.m
@@ -2582,7 +2582,7 @@ withReplyEvent:(NSAppleEventDescriptor *)replyEvent
 				{
 					[browserView setActiveTabToPrimaryTab];
 					if (self.selectedArticle == nil)
-						[articleController ensureSelectedArticle:NO];
+						[articleController ensureSelectedArticle];
 					[mainWindow makeFirstResponder:(self.selectedArticle != nil) ? [browserView primaryTabItemView].mainView : foldersTree.mainView];
 					return YES;
 				}
@@ -3069,7 +3069,7 @@ withReplyEvent:(NSAppleEventDescriptor *)replyEvent
 		[articleController markAllFoldersReadByArray:foldersTree.selectedFolders];
 		NSInteger nextFolderInTree = [foldersTree nextFolderWithUnread:articleController.currentFolderId];
 		[foldersTree selectFolder:nextFolderInTree];
-        [articleController ensureSelectedArticle:NO];
+        [articleController ensureSelectedArticle];
 	}
 }
 

src/ArticleController.h
@@ -84,7 +84,7 @@
 -(void)refilterArrayOfArticles;
 @property (nonatomic, readonly, copy) NSString *sortColumnIdentifier;
 @property (nonatomic, readonly) BOOL sortIsAscending;
--(void)ensureSelectedArticle:(BOOL)singleSelection;
+-(void)ensureSelectedArticle;
 -(void)sortByIdentifier:(NSString *)columnName;
 -(void)sortAscending:(BOOL)newAscending;
 -(void)deleteArticlesByArray:(NSArray *)articleArray;

commit 99858705a07a20831fa3e211aa5e0d7f20002c1a

Fix for https://forums.cocoaforge.com/viewtopic.php?f=18&t=27010
This bug was introduced in 87a2c7cb080b53d6a884d616029fe21650e040dd

src/ArticleBaseView.h
@@ -29,7 +29,6 @@
 	@property (nonatomic, readonly, strong) Article *selectedArticle;
 	@property (nonatomic, readonly, copy) NSArray *markedArticleRange;
 	-(void)saveTableSettings;
-	-(void)selectPreviousArticle;
 	-(void)ensureSelectedArticle;
 	-(void)startLoadIndicator;
 	-(void)stopLoadIndicator;

src/ArticleController.m
@@ -615,11 +615,42 @@
 	NSMutableArray * folderArrayCopy = [NSMutableArray arrayWithArray:folderArrayOfArticles];
 	__block BOOL needReload = NO;
 	
+	NSString * guidToSelect = nil;
+	
     // if we mark deleted, mark also read and unflagged
 	if (deleteFlag) {
 	    [self innerMarkReadByRefsArray:articleArray readFlag:YES];
         [self innerMarkFlaggedByArray:articleArray flagged:NO];
-		[mainArticleView selectPreviousArticle];
+		
+		Article * firstArticle = articleArray.firstObject;
+		if (firstArticle != nil) // Should always be true
+		{
+			// We want to select the next non-deleted article
+			NSUInteger articleIndex = [currentArrayOfArticles indexOfObject:firstArticle];
+			if (articleIndex != NSNotFound)
+			{
+				NSUInteger count = currentArrayOfArticles.count;
+				for (NSUInteger i = articleIndex + 1; i < count; ++i)
+				{
+					Article * nextArticle = [currentArrayOfArticles objectAtIndex:i];
+					if (![articleArray containsObject:nextArticle])
+					{
+						guidToSelect = nextArticle.guid;
+						break;
+					}
+				}
+				
+				// Otherwise, we want to select the previous article.
+				if (guidToSelect == nil && articleIndex > 0)
+				{
+					Article * nextArticle = [currentArrayOfArticles objectAtIndex:articleIndex - 1];
+					guidToSelect = nextArticle.guid;
+				}
+				
+				// Deselect all now, select article after refresh
+				[mainArticleView scrollToArticle:nil];
+			}
+		}
 	}
 
 	// Iterate over every selected article in the table and set the deleted
@@ -651,9 +682,17 @@
 	{
 		[mainArticleView refreshFolder:MA_Refresh_RedrawList];
 		if (currentArrayOfArticles.count > 0u)
+		{
+			if (guidToSelect != nil)
+			{
+				[mainArticleView scrollToArticle:guidToSelect];
+			}
 			[mainArticleView ensureSelectedArticle];
+		}
 		else
+		{
 			[NSApp.mainWindow makeFirstResponder:foldersTree.mainView];
+		}
 	}
 }
 
@@ -667,7 +706,37 @@
 	NSMutableArray * folderArrayCopy = [NSMutableArray arrayWithArray:folderArrayOfArticles];
 	
 	[self innerMarkReadByRefsArray:articleArray readFlag:YES];
-	[mainArticleView selectPreviousArticle];
+	
+	NSString * guidToSelect = nil;
+	Article * firstArticle = articleArray.firstObject;
+	if (firstArticle != nil) // Should always be true
+	{
+		// We want to select the next non-deleted article
+		NSUInteger articleIndex = [currentArrayOfArticles indexOfObject:firstArticle];
+		if (articleIndex != NSNotFound)
+		{
+			NSUInteger count = currentArrayOfArticles.count;
+			for (NSUInteger i = articleIndex + 1; i < count; ++i)
+			{
+				Article * nextArticle = [currentArrayOfArticles objectAtIndex:i];
+				if (![articleArray containsObject:nextArticle])
+				{
+					guidToSelect = nextArticle.guid;
+					break;
+				}
+			}
+			
+			// Otherwise, we want to select the previous article.
+			if (guidToSelect == nil && articleIndex > 0)
+			{
+				Article * nextArticle = [currentArrayOfArticles objectAtIndex:articleIndex - 1];
+				guidToSelect = nextArticle.guid;
+			}
+			
+			// Deselect all now, select article after refresh
+			[mainArticleView scrollToArticle:nil];
+		}
+	}
 
 	// Iterate over every selected article in the table and remove it from
 	// the database.
@@ -685,6 +754,10 @@
 
 	// Ensure there's a valid selection
     if (currentArrayOfArticles.count > 0u) {
+		if (guidToSelect != nil)
+		{
+			[mainArticleView scrollToArticle:guidToSelect];
+		}
 		[mainArticleView ensureSelectedArticle];
     } else {
 		[NSApp.mainWindow makeFirstResponder:foldersTree.mainView];

src/ArticleListView.m
@@ -508,22 +508,6 @@ static const CGFloat MA_Minimum_Article_Pane_Dimension = 80;
 	}
 }
 
-/* selectPreviousArticle
- * Select the previous article in the list if possible.
- */
--(void)selectPreviousArticle
-{
-	NSUInteger nextRow = articleList.selectedRowIndexes.firstIndex;
-	if (nextRow != NSNotFound && nextRow > 0)
-	{
-		[self makeRowSelectedAndVisible:--nextRow];
-	}
-	else
-	{
-		[articleList deselectAll:self];
-	}
-}
-
 /* ensureSelectedArticle
  * Ensure that there is a selected article and that it is visible.
  */

src/UnifiedDisplayView.m
@@ -379,22 +379,6 @@
 	}
 }
 
-/* selectPreviousArticle
- * Select the previous article in the list if possible.
- */
--(void)selectPreviousArticle
-{
-	NSUInteger nextRow = articleList.selectedRowIndexes.firstIndex;
-	if (nextRow != NSNotFound && nextRow > 0)
-	{
-		[self makeRowSelectedAndVisible:--nextRow];
-	}
-	else
-	{
-		[articleList deselectAll:self];
-	}
-}
-
 /* ensureSelectedArticle
  * Ensure that there is a selected article and that it is visible.
  */