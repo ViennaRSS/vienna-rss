9cb72b8d5e0551ec0d9cc167776bb7869efdca03
Eitot <Eitot@users.noreply.github.com>
2018-04-15 14:44:51 +0200

    Move scripts to Scripts/ directory

--- Scripts/Make-ID-Template.sh
+++ Scripts/Make-ID-Template.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [ ! -f configs/CS-ID.xcconfig ]; then
-    cat <<EOF > configs/CS-ID.xcconfig
+if [ ! -f Resources/CS-ID.xcconfig ]; then
+    cat <<EOF > Resources/CS-ID.xcconfig
 // Global settings for Code Signing
 
 CODE_SIGN_IDENTITY = 
 PRIVATE_KEY_PATH = 
 
-CODE_SIGN_REQUIREMENTS_PATH = $(SRCROOT)/signing/codesignrequirement.rqset
+CODE_SIGN_REQUIREMENTS_PATH = Resources/codesignrequirement.rqset
 // CODE_SIGN_RESOURCE_RULES_PATH = $(SRCROOT)/signing/ResourceRules.plist
 
 EOF
