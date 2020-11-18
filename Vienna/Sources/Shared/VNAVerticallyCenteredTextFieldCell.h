ed1452574d0f772bd0d5567fd5516848f144e505
Barijaona Ramaholimihaso <github.com@barijaona.com>
2020-11-18 10:01:05 +0300

    Beautify the code

--- Vienna/Sources/Shared/VNAVerticallyCenteredTextFieldCell.h
+++ Vienna/Sources/Shared/VNAVerticallyCenteredTextFieldCell.h
@@ -2,7 +2,8 @@
 //  VNAVerticallyCenteredTextFieldCell.h
 //  Vertically centered NSTextFieldCell
 //
-//  Idea by Jacob Egger and Matt Bell http://stackoverflow.com/questions/1235219/is-there-a-right-way-to-have-nstextfieldcell-draw-vertically-centered-text
+//  Idea by Jacob Egger and Matt Bell
+//  http://stackoverflow.com/questions/1235219/is-there-a-right-way-to-have-nstextfieldcell-draw-vertically-centered-text
 //  Modified by Barijaona Ramaholimihaso
 //
 
