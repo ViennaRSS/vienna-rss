065b1127908e29c3245eb7dda1ceb6619f82e534
Eitot <Eitot@users.noreply.github.com>
2020-11-27 01:10:29 +0100

    Remove notification badge preference
    This preference is handled by System Preferences. Removing it from Vienna avoids confusion and conflicting preferences.

--- Vienna/Sources/Application/AppController.m
+++ Vienna/Sources/Application/AppController.m
@@ -1475,17 +1475,8 @@ withReplyEvent:(NSAppleEventDescriptor *)replyEvent
  */
 -(void)updateNewArticlesNotification
 {
-	if (([Preferences standardPreferences].newArticlesNotification
-		& VNANewArticlesNotificationBadge) == 0)
-	{
-		// Remove the badge if there was one.
-		[NSApp.dockTile setBadgeLabel:nil];
-	}
-	else
-	{
-		lastCountOfUnread = -1;	// Force an update
-		[self showUnreadCountOnApplicationIconAndWindowTitle];
-	}
+	lastCountOfUnread = -1;	// Force an update
+	[self showUnreadCountOnApplicationIconAndWindowTitle];
 }
 
 - (void)handleUpdateUnreadCount:(NSNotification *)nc
@@ -1522,11 +1513,6 @@ withReplyEvent:(NSAppleEventDescriptor *)replyEvent
             self.mainWindow.subtitle = [NSString stringWithFormat:@"%li %@", (long)currentCountOfUnread, NSLocalizedString(@"Unread", nil)];
         } else {
             self.mainWindow.title = [NSString stringWithFormat:@"%@ (%li %@)", self.appName, (long)currentCountOfUnread, NSLocalizedString(@"Unread", nil)];
-        }
-
-        // Exit now if we're not showing the unread count on the application icon
-        if (([Preferences standardPreferences].newArticlesNotification & VNANewArticlesNotificationBadge) ==0) {
-            return;
         }
 
         NSString *countdown = [NSString stringWithFormat:@"%li", (long)currentCountOfUnread];
