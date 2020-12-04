472fbe514c4dcc1837957290d8cc6ae7580f051c
nielspl <ezzora>
2020-12-04 09:58:49 +0100

    move objective-c includes to tests bridging header

--- Vienna Tests/Vienna Tests-Bridging-Header.h
+++ Vienna Tests/Vienna Tests-Bridging-Header.h
@@ -2,3 +2,9 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 #import "Vienna-Bridging-Header.h"
+
+#import "Criteria.h"
+#import "Database.h"
+#import "Export.h"
+#import "FoldersTree.h"
+#import "SubscriptionModel.h"
