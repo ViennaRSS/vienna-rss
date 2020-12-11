250916a34b44293c8195b6c14fb93dd5e25d38a2
Barijaona Ramaholimihaso <github.com@barijaona.com>
2020-12-11 08:30:52 +0000

    Fix database update on permanent HTTP redirection
    We were (wrongly) applying the original URL instead of the updated one.

--- Vienna/Sources/Fetching/RefreshManager.m
+++ Vienna/Sources/Fetching/RefreshManager.m
@@ -1078,8 +1078,8 @@ typedef NS_ENUM (NSInteger, Redirect301Status) {
 {
     for (id obj in [self.redirect301WaitQueue reverseObjectEnumerator]) {
         NSURLSessionTask *theConnector = (NSURLSessionTask *)obj;
-        NSMutableURLRequest * originalRequest = (NSMutableURLRequest *)theConnector.originalRequest;
         [self.redirect301WaitQueue removeObject:obj];
+        NSMutableURLRequest * originalRequest = (NSMutableURLRequest *)theConnector.originalRequest;
         ActivityItem *connectorItem = ((NSDictionary *)[originalRequest userInfo])[@"log"];
         [connectorItem appendDetail:NSLocalizedString(@"Redirection attempt treated as temporary for safety concern", nil)];
     }
@@ -1095,9 +1095,9 @@ typedef NS_ENUM (NSInteger, Redirect301Status) {
 {
     for (id obj in [self.redirect301WaitQueue reverseObjectEnumerator]) {
         NSURLSessionTask *theConnector = (NSURLSessionTask *)obj;
-        NSMutableURLRequest * originalRequest = (NSMutableURLRequest *)theConnector.originalRequest;
         [self.redirect301WaitQueue removeObject:obj];
-        NSString * theNewURLString = theConnector.originalRequest.URL.absoluteString;
+        NSString * theNewURLString = theConnector.currentRequest.URL.absoluteString;
+        NSMutableURLRequest * originalRequest = (NSMutableURLRequest *)theConnector.originalRequest;
         Folder * theFolder = (Folder *)((NSDictionary *)[originalRequest userInfo])[@"folder"];
         [[Database sharedManager] setFeedURL:theNewURLString forFolder:theFolder.itemId];
         ActivityItem *connectorItem = ((NSDictionary *)[originalRequest userInfo])[@"log"];
