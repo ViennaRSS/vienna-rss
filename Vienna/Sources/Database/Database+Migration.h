28f0f13ab281731d33ede87d90a5cc0cf35073cd
Eitot <Eitot@users.noreply.github.com>
2020-12-28 18:00:30 +0100

    Turn VNADatabaseMigration class into Database category

--- Vienna/Sources/Database/Database+Migration.h
+++ Vienna/Sources/Database/Database+Migration.h
@@ -1,15 +1,14 @@
 //
-//  VNADatabaseMigration.h
+//  Database+Migration.h
 //  Vienna
 //
 //  Created by Joshua Pore on 3/03/2015.
 //  Copyright (c) 2015 The Vienna Project. All rights reserved.
 //
 
-@import Foundation;
-@import FMDB;
+#import "Database.h"
 
-@interface VNADatabaseMigration : NSObject
+@interface Database (Migration)
 
 + (void)migrateDatabase:(FMDatabase *)db fromVersion:(NSInteger)fromVersion;
 
