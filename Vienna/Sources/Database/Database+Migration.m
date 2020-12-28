d144f2bb3448e0d6d63a9819f4966740cf2b3b16
Eitot <Eitot@users.noreply.github.com>
2020-12-28 15:48:36 +0100

    Reformat Database+Migration category

--- Vienna/Sources/Database/Database+Migration.m
+++ Vienna/Sources/Database/Database+Migration.m
@@ -2,163 +2,209 @@
 //  Database+Migration.m
 //  Vienna
 //
-//  Created by Joshua Pore on 3/03/2015.
-//  Copyright (c) 2015 The Vienna Project. All rights reserved.
+//  Copyright 2015 Joshua Pore
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import "Database+Migration.h"
-#import "Preferences.h"
+
 #import "Constants.h"
+#import "Preferences.h"
 
 @implementation Database (Migration)
 
-
-/*!
- *  Migrate the Vienna database schema
- *
- *  @param fromVer the version we want to migrate from
- */
-+ (void)migrateDatabase:(FMDatabase *)db fromVersion:(NSInteger)fromVersion {
-    
-    switch (fromVersion+1) {
++ (void)migrateDatabase:(FMDatabase *)database
+            fromVersion:(NSInteger)previousVersion
+{
+    switch (previousVersion + 1) {
         case 13: {
-            // Upgrade to rev 13.
-            // Add createddate field to the messages table and initialise it to a date in the past.
-            // Create an index on the message_id column.
+            // Add createddate field to the messages table and initialise it to
+            // a date in the past. Create an index on the message_id column.
 
-            [db executeUpdate:@"alter table messages add column createddate"];
-            [db executeUpdate:@"update messages set createddate=?", @([NSDate distantPast].timeIntervalSince1970)];
-            [db executeUpdate:@"create index messages_message_idx on messages (message_id)"];
-            db.userVersion = (uint32_t)13;
+            [database executeUpdate:@"ALTER TABLE messages "
+                                     "ADD COLUMN createddate"];
+            NSTimeInterval interval = NSDate.distantPast.timeIntervalSince1970;
+            [database executeUpdate:@"UPDATE messages SET createddate = ?",
+                                    @(interval)];
+            [database executeUpdate:@"CREATE INDEX messages_message_idx "
+                                     "ON messages (message_id)"];
+            database.userVersion = (uint32_t)13;
 
             NSLog(@"Updated database schema to version 13.");
         }
         case 14: {
-            // Upgrade to rev 14.
-            // Add next_sibling and next_child columns to folders table and first_folder column to info table to allow for manual sorting.
-            // Initialize all values to 0. The correct values will be set by -[FoldersTree setManualSortOrderForNode:].
-            // Make sure that all parent_id values are integers rather than strings, because previous versions of setParent:forFolder:
-            // set them as strings.
-            
-            [db executeUpdate:@"alter table info add column first_folder"];
-            [db executeUpdate:@"update info set first_folder=0"];
-            
-            [db executeUpdate:@"alter table folders add column next_sibling"];
-            [db executeUpdate:@"update folders set next_sibling=0"];
-            
-            [db executeUpdate:@"alter table folders add column first_child"];
-            [db executeUpdate:@"update folders set first_child=0"];
-            
-            [[Preferences standardPreferences] setFoldersTreeSortMethod:VNAFolderSortByName];
-            
-            FMResultSet * results = [db executeQuery:@"select folder_id, parent_id from folders"];
-            while([results next])
-            {
+            // Add next_sibling and next_child columns to folders table and
+            // first_folder column to info table to allow for manual sorting.
+            // Initialize all values to 0. The correct values will be set by
+            // -[FoldersTree setManualSortOrderForNode:]. Make sure that all
+            // parent_id values are integers rather than strings, because
+            // previous versions of setParent:forFolder: set them as strings.
+
+            [database executeUpdate:@"ALTER TABLE info "
+                                     "ADD COLUMN first_folder"];
+            [database executeUpdate:@"UPDATE info SET first_folder = 0"];
+
+            [database executeUpdate:@"ALTER TABLE folders "
+                                     "ADD COLUMN next_sibling"];
+            [database executeUpdate:@"UPDATE folders SET next_sibling = 0"];
+
+            [database executeUpdate:@"ALTER TABLE folders "
+                                     "ADD COLUMN first_child"];
+            [database executeUpdate:@"UPDATE folders SET first_child = 0"];
+
+            Preferences *preferences = [Preferences standardPreferences];
+            [preferences setFoldersTreeSortMethod:VNAFolderSortByName];
+
+            FMResultSet *results = [database executeQuery:@"SELECT folder_id, "
+                                                           "parent_id "
+                                                           "FROM folders"];
+            while ([results next]) {
                 NSNumber *folderId = [results objectForColumn:@"folder_id"];
                 NSNumber *parentId = [results objectForColumn:@"parent_id"];
-                [db executeUpdate:@"update folders set parent_id=? where folder_id=?",
-                 parentId, folderId];
+                [database executeUpdate:@"UPDATE folders "
+                                         "SET parent_id = ? "
+                                         "WHERE folder_id = ?",
+                                        parentId,
+                                        folderId];
             }
             [results close];
-            db.userVersion = (uint32_t)14;
-            
+            database.userVersion = (uint32_t)14;
+
             NSLog(@"Updated database schema to version 14.");
         }
         case 15: {
-            // Upgrade to rev 15.
-            // Move the folders tree sort method preference to the database, so that it can survive deletion of the preferences file.
-            // Do not disturb the manual sort order, if it exists.
-            
-            [db executeUpdate:@"alter table info add column folder_sort"];
-            NSInteger oldFoldersTreeSortMethod = [Preferences standardPreferences].foldersTreeSortMethod;
-            [db executeUpdate:@"update info set folder_sort=?", @(oldFoldersTreeSortMethod)];
-            db.userVersion = (uint32_t)15;
-            
+            // Move the folders tree sort method preference to the database, so
+            // that it can survive deletion of the preferences file. Do not
+            // disturb the manual sort order, if it exists.
+
+            [database executeUpdate:@"ALTER TABLE info ADD COLUMN folder_sort"];
+            Preferences *preferences = [Preferences standardPreferences];
+            NSInteger oldSortMethod = preferences.foldersTreeSortMethod;
+            [database executeUpdate:@"UPDATE info SET folder_sort = ?",
+                                    @(oldSortMethod)];
+            database.userVersion = (uint32_t)15;
+
             NSLog(@"Updated database schema to version 15.");
         }
         case 16: {
-            // Upgrade to rev 16.
-            // Add revised_flag to messages table, and initialize all values to 0.
-            
-            [db executeUpdate:@"alter table messages add column revised_flag"];
-            [db executeUpdate:@"update messages set revised_flag=0"];
-            db.userVersion = (uint32_t)16;
+            // Add revised_flag to messages table, and initialize all values to
+            // 0.
+
+            [database executeUpdate:@"ALTER TABLE messages "
+                                     "ADD COLUMN revised_flag"];
+            [database executeUpdate:@"UPDATE messages SET revised_flag = 0"];
+            database.userVersion = (uint32_t)16;
 
             NSLog(@"Updated database schema to version 16.");
         }
         case 17: {
-            // Upgrade to rev 17.
-            // Add hasenclosure_flag, enclosuredownloaded_flag and enclosure to messages table, and initialize stuff.
- 
-            [db executeUpdate:@"alter table messages add column hasenclosure_flag"];
-            [db executeUpdate:@"update messages set hasenclosure_flag=0"];
-            [db executeUpdate:@"alter table messages add column enclosure"];
-            [db executeUpdate:@"update messages set enclosure=''"];
-            [db executeUpdate:@"alter table messages add column enclosuredownloaded_flag"];
-            [db executeUpdate:@"update messages set enclosuredownloaded_flag=0"];
-            db.userVersion = (uint32_t)17;
+            // Add hasenclosure_flag, enclosuredownloaded_flag and enclosure to
+            // messages table, and initialize stuff.
+
+            [database executeUpdate:@"ALTER TABLE messages "
+                                     "ADD COLUMN hasenclosure_flag"];
+            [database executeUpdate:@"UPDATE messages "
+                                     "SET hasenclosure_flag = 0"];
+            [database executeUpdate:@"ALTER TABLE messages "
+                                     "ADD COLUMN enclosure"];
+            [database executeUpdate:@"UPDATE messages SET enclosure = ''"];
+            [database executeUpdate:@"ALTER TABLE messages "
+                                     "ADD COLUMN enclosuredownloaded_flag"];
+            [database executeUpdate:@"UPDATE messages "
+                                     "SET enclosuredownloaded_flag = 0"];
+            database.userVersion = (uint32_t)17;
 
             NSLog(@"Updated database schema to version 17.");
         }
         case 18: {
-            // Upgrade to rev 18.
             // Add table all message guids.
 
-            [db executeUpdate:@"create table rss_guids as select message_id, folder_id from messages"];
-            [db executeUpdate:@"create index rss_guids_idx on rss_guids (folder_id)"];
-            db.userVersion = (uint32_t)18;
+            [database executeUpdate:@"CREATE TABLE rss_guids "
+                                     "AS SELECT message_id, folder_id "
+                                     "FROM messages"];
+            [database executeUpdate:@"CREATE INDEX rss_guids_idx "
+                                     "ON rss_guids (folder_id)"];
+            database.userVersion = (uint32_t)18;
 
             NSLog(@"Updated database schema to version 18.");
         }
         case 19: {
-            // Upgrade to rev 19.
-            // Update the Vienna Developer's blog RSS URL after we changed from .org to .com
+            // Update the Vienna Developer's blog RSS URL after we changed from
+            // .org to .com
 
-            FMResultSet *results = [db executeQuery:@"SELECT folder_id FROM rss_folders WHERE feed_url LIKE ?", @"%%vienna-rss.org%%"];
+            FMResultSet *results =
+                [database executeQuery:@"SELECT folder_id "
+                                        "FROM rss_folders "
+                                        "WHERE feed_url LIKE ?",
+                                       @"%%vienna-rss.org%%"];
 
-            if([results next]) {
+            if ([results next]) {
                 int viennaFolderId = [results intForColumn:@"folder_id"];
-                [db executeUpdate:@"UPDATE rss_folders SET feed_url=?, home_page=? WHERE folder_id=?",
-                 @"http://www.vienna-rss.com/?feed=rss2",
-                 @"http://www.vienna-rss.com",
-                 @(viennaFolderId)];
+                [database executeUpdate:@"UPDATE rss_folders "
+                                         "SET feed_url = ?, home_page = ? "
+                                         "WHERE folder_id = ?",
+                                        @"http://www.vienna-rss.com/?feed=rss2",
+                                        @"http://www.vienna-rss.com",
+                                        @(viennaFolderId)];
             }
             [results close];
-            db.userVersion = (uint32_t)19;
+            database.userVersion = (uint32_t)19;
             NSLog(@"Updated database schema to version 19.");
         }
         case 20: {
-            // Upgrade to rev 20.
-            // Update the Vienna Developer's blog RSS URL after moved to github pages
-            
-            FMResultSet *results = [db executeQuery:@"SELECT folder_id FROM rss_folders WHERE feed_url LIKE ?", @"%%www.vienna-rss.com/?feed=rss2%%"];
-            
-            if([results next]) {
+            // Update the Vienna Developer's blog RSS URL after moved to github
+            // pages
+
+            FMResultSet *results =
+                [database executeQuery:@"SELECT folder_id "
+                                        "FROM rss_folders "
+                                        "WHERE feed_url LIKE ?",
+                                       @"%%www.vienna-rss.com/?feed=rss2%%"];
+
+            if ([results next]) {
                 int viennaFolderId = [results intForColumn:@"folder_id"];
-                [db executeUpdate:@"UPDATE rss_folders SET feed_url=? WHERE folder_id=?",
-                 @"https://www.vienna-rss.com/feed.xml",
-                 @(viennaFolderId)];
+                [database executeUpdate:@"UPDATE rss_folders "
+                                         "SET feed_url = ? "
+                                         "WHERE folder_id = ?",
+                                        @"https://www.vienna-rss.com/feed.xml",
+                                        @(viennaFolderId)];
             }
             [results close];
-            db.userVersion = (uint32_t)20;
+            database.userVersion = (uint32_t)20;
             NSLog(@"Updated database schema to version 20.");
         }
         case 21: {
             // Removes line-breaks and tabs from author strings.
 
-            FMResultSet *results = [db executeQuery:@"SELECT message_id, sender FROM messages WHERE TRIM(sender) > ''"];
+            FMResultSet *results =
+                [database executeQuery:@"SELECT message_id, sender "
+                                        "FROM messages "
+                                        "WHERE TRIM(sender) > ''"];
 
             // Create a character set that contains new-line characters and the
             // tab character (Unicode U+0009).
-            NSString *tabCharacter = [NSString stringWithCharacters:(UniChar[]){0x0009}
+            NSMutableCharacterSet *characterSet = nil;
+            characterSet = NSMutableCharacterSet.newlineCharacterSet;
+            unichar character[] = {0x0009};
+            NSString *tabCharacter = [NSString stringWithCharacters:character
                                                              length:1];
-            NSMutableCharacterSet *characterSet = NSMutableCharacterSet.newlineCharacterSet;
             [characterSet addCharactersInString:tabCharacter];
 
             while ([results next]) {
                 NSString *oldSender = [results stringForColumn:@"sender"];
 
-                if (!oldSender || oldSender.length == 0 ) {
+                if (!oldSender || oldSender.length == 0) {
                     continue;
                 }
 
@@ -178,15 +224,17 @@
                 }
 
                 NSString *messageID = [results stringForColumn:@"message_id"];
-                [db executeUpdate:@"UPDATE messages SET sender=? WHERE message_id=?",
-                 newSender, messageID];
+                [database executeUpdate:@"UPDATE messages "
+                                         "SET sender= ? "
+                                         "WHERE message_id = ?",
+                                        newSender,
+                                        messageID];
             }
 
-            db.userVersion = (uint32_t)21;
+            database.userVersion = (uint32_t)21;
             NSLog(@"Updated database schema to version 21.");
         }
     }
-    
 }
 
 @end
