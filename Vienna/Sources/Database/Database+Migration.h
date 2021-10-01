bfbeaf7ebdee1538f26d1f78beb003559e84e0e7
Barijaona Ramaholimihaso <github.com@barijaona.com>
2021-10-01 08:56:57 +0300

    Revert "Reformat Database+Migration category"
    This reverts commit d144f2bb3448e0d6d63a9819f4966740cf2b3b16.

--- Vienna/Sources/Database/Database+Migration.h
+++ Vienna/Sources/Database/Database+Migration.h
@@ -2,29 +2,14 @@
 //  Database+Migration.h
 //  Vienna
 //
-//  Copyright 2015 Joshua Pore
-//
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-//
-//  https://www.apache.org/licenses/LICENSE-2.0
-//
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
+//  Created by Joshua Pore on 3/03/2015.
+//  Copyright (c) 2015 The Vienna Project. All rights reserved.
 //
 
 #import "Database.h"
 
 @interface Database (Migration)
 
-/// Migrates the Vienna database schema.
-/// @param database The database to migrate.
-/// @param previousVersion The version to migrate from.
-+ (void)migrateDatabase:(FMDatabase *)database
-            fromVersion:(NSInteger)previousVersion;
++ (void)migrateDatabase:(FMDatabase *)db fromVersion:(NSInteger)fromVersion;
 
 @end
