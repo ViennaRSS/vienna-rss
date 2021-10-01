5f07d2dc7517dae10904fec81badc820239d9c82
Barijaona Ramaholimihaso <github.com@barijaona.com>
2021-10-01 08:56:57 +0300

    Revert "Remove availability workarounds for macOS 10.10 Yosemite"
    This reverts commit 2c3cd0685a77d592dddc84027b88dc2857439e50.

--- Vienna/Sources/Fetching/RefreshManager.m
+++ Vienna/Sources/Fetching/RefreshManager.m
@@ -83,8 +83,12 @@ typedef NS_ENUM (NSInteger, Redirect301Status) {
         networkQueue.name = @"VNAHTTPSession queue";
         networkQueue.maxConcurrentOperationCount = [[Preferences standardPreferences] integerForKey:MAPref_ConcurrentDownloads];
         NSString * osVersion;
-        NSOperatingSystemVersion version = [NSProcessInfo processInfo].operatingSystemVersion;
-        osVersion = [NSString stringWithFormat:@"%ld_%ld_%ld", version.majorVersion, version.minorVersion, version.patchVersion];
+        if (@available(macOS 10.10, *)) {
+            NSOperatingSystemVersion version = [NSProcessInfo processInfo].operatingSystemVersion;
+            osVersion = [NSString stringWithFormat:@"%ld_%ld_%ld", version.majorVersion, version.minorVersion, version.patchVersion];
+        } else {
+            osVersion = @"10_9_x";
+        }
         Preferences * prefs = [Preferences standardPreferences];
         NSString *name = prefs.userAgentName;
 
