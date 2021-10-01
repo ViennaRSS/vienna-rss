ee4692586217e3dda28bafceedce3cc2f58a9790
Barijaona Ramaholimihaso <github.com@barijaona.com>
2021-10-01 08:56:57 +0300

    Revert "Add minSize and maxSize properties to toolbar items"
    This reverts commit fec8f9a491be8d9b923d13d277eea168c5395678.

--- Vienna/Sources/Main window/MainWindowController.swift
+++ Vienna/Sources/Main window/MainWindowController.swift
@@ -180,7 +180,6 @@ extension MainWindowController: NSToolbarDelegate {
             } else {
                 item = NSToolbarItem(itemIdentifier: itemIdentifier)
                 item.view = NSSearchField()
-                item.maxSize.width = 210
                 item.visibilityPriority = .high
                 toolbarSearchField = item.view as? NSSearchField
             }
