0dd0913bc07bb1f10e9df5d6630e4a11011679e6
Eitot <Eitot@users.noreply.github.com>
2021-11-12 02:53:28 +0100

    Remove unused NSNoficationCenter extension methods

--- Vienna/Sources/Shared/NSNotificationAdditions.h
+++ Vienna/Sources/Shared/NSNotificationAdditions.h
@@ -2,11 +2,7 @@
 
 @interface NSNotificationCenter (NSNotificationCenterAdditions)
 
-- (void)vna_postNotificationOnMainThread:(NSNotification *)notification;
 - (void)vna_postNotificationOnMainThreadWithName:(NSString *)name
                                           object:(id)object;
-- (void)vna_postNotificationOnMainThreadWithName:(NSString *)name
-                                          object:(id)object
-                                        userInfo:(NSDictionary *)userInfo;
 
 @end
--- Vienna/Sources/Shared/NSNotificationAdditions.m
+++ Vienna/Sources/Shared/NSNotificationAdditions.m
@@ -2,24 +2,10 @@
 
 @implementation NSNotificationCenter (NSNotificationCenterAdditions)
 
-- (void)vna_postNotificationOnMainThread:(NSNotification *)notification {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [self postNotification:notification];
-    });
-}
-
 - (void)vna_postNotificationOnMainThreadWithName:(NSString *)name
                                           object:(id)object {
     dispatch_async(dispatch_get_main_queue(), ^{
         [self postNotificationName:name object:object];
-    });
-}
-
-- (void)vna_postNotificationOnMainThreadWithName:(NSString *)name
-                                          object:(id)object
-                                        userInfo:(NSDictionary *)userInfo {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [self postNotificationName:name object:object userInfo:userInfo];
     });
 }
 
