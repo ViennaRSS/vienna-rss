e173844895311ae847e1c31a367bc8a1b419f4f0
Tassilo Karge <tassilo.karge@web.de>
2021-11-14 12:22:34 +0100

    make main thread wait for JS callback, only execute js if page was successfully loaded
    synchronization has to be in place due to animation of the address bar.
    
    The javascript callback will only be executed if the page has loaded properly.
    
    This might solve the problems observed in #1502 while keeping the original way this worked

--- Vienna/Sources/Main window/BrowserTab+RSSSource.swift
+++ Vienna/Sources/Main window/BrowserTab+RSSSource.swift
@@ -72,23 +72,23 @@ extension BrowserTab: RSSSource {
     }
 
     func handleNavigationEndRss(success: Bool) {
+        guard success else {
+            self.rssUrls = []
+            return
+        }
         // use javascript to detect RSS feed link
         // TODO: deal with multiple links
-        let group = DispatchGroup()
-        group.enter()
-        self.webView.evaluateJavaScript(BrowserTab.extractRssLinkScript) { result, error in
-            if error == nil, let result = result as? [String] {
-                // RSS feed link(s) detected
-                self.rssUrls = result.compactMap { URL(string: $0 as String) }
-            } else {
-                // error or no rss url available
-                self.rssUrls = []
+        waitForAsyncExecution { finishHandler in
+            self.webView.evaluateJavaScript(BrowserTab.extractRssLinkScript) { result, error in
+                if error == nil, let result = result as? [String] {
+                    // RSS feed link(s) detected
+                    self.rssUrls = result.compactMap { URL(string: $0 as String) }
+                } else {
+                    // error or no rss url available
+                    self.rssUrls = []
+                }
+                finishHandler()
             }
-            group.leave()
-        }
-
-        group.notify(queue: DispatchQueue.main) {
-            self.updateAddressBarButtons()
         }
     }
 }
