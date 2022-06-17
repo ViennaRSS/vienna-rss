41b1172637370d3c2293c0622521b3ac82a4611e
Eitot <Eitot@users.noreply.github.com>
2022-06-17 12:14:44 +0200

    Add preference to hide the enclosure view

--- Vienna/Sources/Shared/Constants.h
+++ Vienna/Sources/Shared/Constants.h
@@ -82,6 +82,7 @@ extern NSString * MAPref_AlwaysAcceptBetas;
 extern NSString * MAPref_UserAgentName;
 extern NSString *const MAPref_UseRelativeDates;
 extern NSString * const MAPref_ShowDetailsOnFeedCredentialsDialog;
+extern NSString * const MAPref_ShowEnclosureBar;
 
 // Deprecated defaults keys
 extern NSString *const MAPref_Deprecated_ArticleListFont;
