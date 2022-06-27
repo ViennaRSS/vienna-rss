ViennaRSS/vienna-rss

commit e720d108e70911301930a6b68d2faf9955051aed

Enable miscellaneous warnings and fix them

Vienna/Sources/Fetching/RefreshManager.m
@@ -20,7 +20,7 @@
 
 #import "RefreshManager.h"
 
-#import <os/log.h>
+@import os.log;
 
 #import "FeedCredentials.h"
 #import "ActivityItem.h"
@@ -416,7 +416,7 @@ typedef NS_ENUM (NSInteger, Redirect301Status) {
                         [aItem appendDetail:[NSString stringWithFormat:NSLocalizedString(@"Folder image retrieved from %@",
                                                                                          nil), myRequest.URL]];
 
-                        NSString * byteCount = [NSByteCountFormatter stringFromByteCount:data.length
+                        NSString * byteCount = [NSByteCountFormatter stringFromByteCount:(long long)data.length
                                                                               countStyle:NSByteCountFormatterCountStyleFile];
                         [aItem appendDetail:[NSString stringWithFormat:NSLocalizedString(@"%@ received",
                                                                                          @"Number of bytes received, e.g. 1 MB received"),
@@ -649,21 +649,15 @@ typedef NS_ENUM (NSInteger, Redirect301Status) {
 
     // Check whether this is an HTML redirect. If so, create a new connection using
     // the redirect.
-
     NSString * redirectURL = [self getRedirectURL:receivedData];
-
-
-    if (redirectURL != nil) {
-        if ([redirectURL isEqualToString:url.absoluteString]) {
-            // To prevent an infinite loop, don't redirect to the same URL.
-            [connectorItem appendDetail:[NSString stringWithFormat:NSLocalizedString(@"Improper infinitely looping URL redirect to %@",
-                                                                                     nil), url.absoluteString]];
-        } else {
-            [self refreshFeed:folder fromURL:[NSURL URLWithString:redirectURL] withLog:connectorItem shouldForceRefresh:NO];
-            return;
-        }
+    if (redirectURL && url && [redirectURL isEqualToString:url.absoluteString]) {
+        // To prevent an infinite loop, don't redirect to the same URL.
+        [connectorItem appendDetail:[NSString stringWithFormat:NSLocalizedString(@"Improper infinitely looping URL redirect to %@",
+                                                                                 nil), url.absoluteString]];
+    } else {
+        [self refreshFeed:folder fromURL:[NSURL URLWithString:redirectURL] withLog:connectorItem shouldForceRefresh:NO];
+        return;
     }
-
 
     // Empty data feed is OK if we got HTTP 200
     __block NSUInteger newArticlesFromFeed = 0;

Vienna.xcodeproj/project.pbxproj
@@ -87,7 +87,7 @@
 		37C650801816EBAD3C0D9DBF /* NSURL+CaminoExtensions.m in Sources */ = {isa = PBXBuildFile; fileRef = 37C65C3C01B0F8EE0C0E5A52 /* NSURL+CaminoExtensions.m */; };
 		3A0F2A55258FBB8B0036397F /* TabbedBrowserViewController.xib in Resources */ = {isa = PBXBuildFile; fileRef = 2FDF6FBD218A266A002F77E9 /* TabbedBrowserViewController.xib */; };
 		3A0F2A9E258FCB780036397F /* BrowserTab.xib in Resources */ = {isa = PBXBuildFile; fileRef = 2FDF6FBE218A266A002F77E9 /* BrowserTab.xib */; };
-		3A171C00210B551B00B80FBB /* TRVSURLSessionOperation.m in Sources */ = {isa = PBXBuildFile; fileRef = 3A171BF1210B551B00B80FBB /* TRVSURLSessionOperation.m */; };
+		3A171C00210B551B00B80FBB /* TRVSURLSessionOperation.m in Sources */ = {isa = PBXBuildFile; fileRef = 3A171BF1210B551B00B80FBB /* TRVSURLSessionOperation.m */; settings = {COMPILER_FLAGS = "-Wno-completion-handler -Wno-idiomatic-parentheses"; }; };
 		3A4DBEC71733C207006DD2AB /* ArticleCellView.m in Sources */ = {isa = PBXBuildFile; fileRef = 3A4DBEC61733C206006DD2AB /* ArticleCellView.m */; };
 		3A50011B259A5EF800AA6AAD /* WebViewArticleConverter.swift in Sources */ = {isa = PBXBuildFile; fileRef = 2F88B2AC2545BCA90067EEA6 /* WebViewArticleConverter.swift */; };
 		3A500122259A63CB00AA6AAD /* ArticleConverter.swift in Sources */ = {isa = PBXBuildFile; fileRef = 2FC4A1BE25852C0E005FF227 /* ArticleConverter.swift */; };
@@ -2926,10 +2926,15 @@
 				SWIFT_OPTIMIZATION_LEVEL = "-Onone";
 				SWIFT_VERSION = 5.0;
 				WARNING_CFLAGS = (
+					"-Wcustom-atomic-properties",
 					"-Wextra-semi",
+					"-Wgnu-conditional-omitted-operand",
+					"-Widiomatic-parentheses",
 					"-Wmissing-method-return-type",
+					"-Wmissing-variable-declarations",
 					"-Wunreachable-code-break",
 					"-Wunreachable-code-return",
+					"-Wunused-macros",
 				);
 			};
 			name = Development;
@@ -2962,10 +2967,15 @@
 				SWIFT_OBJC_BRIDGING_HEADER = "Vienna/Sources/Vienna-Bridging-Header.h";
 				SWIFT_VERSION = 5.0;
 				WARNING_CFLAGS = (
+					"-Wcustom-atomic-properties",
 					"-Wextra-semi",
+					"-Wgnu-conditional-omitted-operand",
+					"-Widiomatic-parentheses",
 					"-Wmissing-method-return-type",
+					"-Wmissing-variable-declarations",
 					"-Wunreachable-code-break",
 					"-Wunreachable-code-return",
+					"-Wunused-macros",
 				);
 			};
 			name = Deployment;
@@ -2979,9 +2989,12 @@
 				CLANG_ENABLE_MODULES = YES;
 				CLANG_ENABLE_OBJC_ARC = YES;
 				CLANG_ENABLE_OBJC_WEAK = YES;
+				CLANG_TIDY_BUGPRONE_REDUNDANT_BRANCH_CONDITION = YES;
+				CLANG_TIDY_MISC_REDUNDANT_EXPRESSION = YES;
 				CLANG_WARN_BLOCK_CAPTURE_AUTORELEASING = YES;
 				CLANG_WARN_BOOL_CONVERSION = YES;
 				CLANG_WARN_COMMA = YES;
+				CLANG_WARN_COMPLETION_HANDLER_MISUSE = YES;
 				CLANG_WARN_CONSTANT_CONVERSION = YES;
 				CLANG_WARN_DEPRECATED_OBJC_IMPLEMENTATIONS = YES;
 				CLANG_WARN_EMPTY_BODY = YES;
@@ -3024,9 +3037,12 @@
 				CLANG_ENABLE_MODULES = YES;
 				CLANG_ENABLE_OBJC_ARC = YES;
 				CLANG_ENABLE_OBJC_WEAK = YES;
+				CLANG_TIDY_BUGPRONE_REDUNDANT_BRANCH_CONDITION = YES;
+				CLANG_TIDY_MISC_REDUNDANT_EXPRESSION = YES;
 				CLANG_WARN_BLOCK_CAPTURE_AUTORELEASING = YES;
 				CLANG_WARN_BOOL_CONVERSION = YES;
 				CLANG_WARN_COMMA = YES;
+				CLANG_WARN_COMPLETION_HANDLER_MISUSE = YES;
 				CLANG_WARN_CONSTANT_CONVERSION = YES;
 				CLANG_WARN_DEPRECATED_OBJC_IMPLEMENTATIONS = YES;
 				CLANG_WARN_EMPTY_BODY = YES;

Vienna/Sources/Activity panel/ActivityLog.h
@@ -31,7 +31,7 @@ extern NSNotificationName const activityLogUpdatedNotification;
 /**
  The default activity log.
  */
-@property (class, readonly) ActivityLog *defaultLog;
+@property (class, readonly, nonatomic) ActivityLog *defaultLog;
 
 /**
  An array of activity items.

Vienna/Sources/Activity panel/ActivityLog.m
@@ -46,7 +46,9 @@
 }
 
 - (instancetype)init {
-    if (self = [super init]) {
+    self = [super init];
+
+    if (self) {
         self.log = [NSMutableArray new];
 
         [NSNotificationCenter.defaultCenter addObserver:self

Vienna/Sources/Application/AppController.m
@@ -20,7 +20,7 @@
 
 #import "AppController.h"
 
-#import <os/log.h>
+@import os.log;
 
 #import "AppController+Notifications.h"
 #import "Import.h"
@@ -524,7 +524,6 @@
 			[Preferences standardPreferences].displayStyle = styleName;
 		else
 		{
-			Preferences * prefs = [Preferences standardPreferences];
             [self populateStyleMenu];
 			prefs.displayStyle = styleName;
             runOKAlertPanel(NSLocalizedString(@"Vienna has installed a new style", nil), NSLocalizedString(@"The style \"%@\" has been installed to your Styles folder and added to the Style menu.", nil), styleName);
@@ -881,7 +880,7 @@ withReplyEvent:(NSAppleEventDescriptor *)replyEvent
     }
 	
 	// Launch in the foreground or background as needed
-	NSWorkspaceLaunchOptions lOptions = openLinksInBackground ? (NSWorkspaceLaunchWithoutActivation | NSWorkspaceLaunchDefault) : (NSWorkspaceLaunchDefault | NSWorkspaceLaunchDefault);
+	NSWorkspaceLaunchOptions lOptions = openLinksInBackground ? (NSWorkspaceLaunchWithoutActivation | NSWorkspaceLaunchDefault) : NSWorkspaceLaunchDefault;
 	[[NSWorkspace sharedWorkspace] openURLs:urlArray
 					withAppBundleIdentifier:NULL
 									options:lOptions

Vienna/Sources/Database/Database.h
@@ -36,7 +36,7 @@ extern NSNotificationName const VNADatabaseDidDeleteFolderNotification;
 @property(nonatomic) FMDatabaseQueue * databaseQueue;
 @property (copy, nonatomic) NSString *searchString;
 
-@property (class, readonly) Database *sharedManager NS_SWIFT_NAME(shared);
+@property (class, readonly, nonatomic) Database *sharedManager NS_SWIFT_NAME(shared);
 
 // General database functions
 - (instancetype)initWithDatabaseAtPath:(NSString *)dbPath /*NS_DESIGNATED_INITIALIZER*/;

Vienna/Sources/Database/Database.m
@@ -20,7 +20,7 @@
 
 #import "Database.h"
 
-#import <os/log.h>
+@import os.log;
 
 #import "Database+Migration.h"
 #import "Preferences.h"

Vienna/Sources/Download window/DownloadManager.h
@@ -25,7 +25,7 @@
 
 @interface DownloadManager : NSObject <NSURLSessionDownloadDelegate, WebDownloadDelegate>
 
-@property (class, readonly) DownloadManager *sharedInstance NS_SWIFT_NAME(shared);
+@property (class, readonly, nonatomic) DownloadManager *sharedInstance NS_SWIFT_NAME(shared);
 
 @property (readonly, nonatomic) NSArray<DownloadItem *> *downloadsList;
 @property (readonly, nonatomic) BOOL hasActiveDownloads;

commit 76e7e228625e69ce9aee64ea53a9c712d804a558

Fix `unsupported URL` errors on feed refreshes
The bug was introduced by commit e720d108: attempt to redirect while
redirectURL is nil.

Issue #1587

Vienna/Sources/Fetching/RefreshManager.m
@@ -650,13 +650,15 @@ typedef NS_ENUM (NSInteger, Redirect301Status) {
     // Check whether this is an HTML redirect. If so, create a new connection using
     // the redirect.
     NSString * redirectURL = [self getRedirectURL:receivedData];
-    if (redirectURL && url && [redirectURL isEqualToString:url.absoluteString]) {
-        // To prevent an infinite loop, don't redirect to the same URL.
-        [connectorItem appendDetail:[NSString stringWithFormat:NSLocalizedString(@"Improper infinitely looping URL redirect to %@",
-                                                                                 nil), url.absoluteString]];
-    } else {
-        [self refreshFeed:folder fromURL:[NSURL URLWithString:redirectURL] withLog:connectorItem shouldForceRefresh:NO];
-        return;
+    if (redirectURL != nil) {
+        if ([redirectURL isEqualToString:url.absoluteString]) {
+            // To prevent an infinite loop, don't redirect to the same URL.
+            [connectorItem appendDetail:[NSString stringWithFormat:NSLocalizedString(@"Improper infinitely looping URL redirect to %@",
+                                                                                     nil), url.absoluteString]];
+        } else {
+            [self refreshFeed:folder fromURL:[NSURL URLWithString:redirectURL] withLog:connectorItem shouldForceRefresh:NO];
+            return;
+        }
     }
 
     // Empty data feed is OK if we got HTTP 200