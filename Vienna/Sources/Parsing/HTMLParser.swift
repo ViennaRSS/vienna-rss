6174ec2059b268b94e1cd73551efde10c567d3df
Eitot <Eitot@users.noreply.github.com>
2024-12-16 13:59:20 +0100

    Tweak HTMLParser code and add more error logging

--- Vienna/Sources/Parsing/HTMLParser.swift
+++ Vienna/Sources/Parsing/HTMLParser.swift
@@ -63,39 +63,51 @@ class HTMLParser {
             return
         }
 
-        data.withUnsafeBytes { buffer in
-            // According to the UnsafeRawBufferPointer documentation, each byte
-            // is addressed as a UInt8 value.
-            guard let baseAddress = buffer.baseAddress?.assumingMemoryBound(to: UInt8.self) else {
-                os_log("Buffer base address is nil", log: .htmlParser, type: .fault)
-                return
-            }
-
-            let numberOfBytes = Int32(buffer.count)
-
-            // Even if the base address is not nil, the byte count can be 0.
-            guard numberOfBytes > 0 else {
-                os_log("Buffer data count is 0", log: .htmlParser, type: .fault)
-                return
-            }
-
-            // Set up the HTML parser.
+        data.withUnsafeBytes { bufferPointer in
             var handler = htmlParserHandler()
             let parser = Unmanaged.passUnretained(self).toOpaque()
-            let address = UnsafeRawPointer(baseAddress).bindMemory(to: Int8.self, capacity: 1)
+            let baseAddress = bufferPointer.bindMemory(to: CChar.self).baseAddress
+            let numberOfBytes = Int32(bufferPointer.count)
+            let filename: UnsafePointer<CChar>? = nil
             let encoding = xmlDetectCharEncoding(baseAddress, numberOfBytes)
-            parserContext = htmlCreatePushParserCtxt(&handler, parser, address, numberOfBytes, nil, encoding)
-
-            let opts = Int32(HTML_PARSE_RECOVER.rawValue | HTML_PARSE_NOBLANKS.rawValue | HTML_PARSE_NONET.rawValue)
-            htmlCtxtUseOptions(parserContext, opts)
-
-            // Parse the document.
-            htmlParseDocument(parserContext)
-
-            // Clean up the parser.
-            htmlFreeParserCtxt(parserContext)
-            self.parserContext = nil
+            parserContext = htmlCreatePushParserCtxt(
+                &handler,
+                parser,
+                baseAddress,
+                numberOfBytes,
+                filename,
+                encoding
+            )
         }
+
+        // HTML_PARSE_NONET is unimplemented and will result in a non-zero
+        // return code of htmlCtxtUseOptions(_:_:).
+        let opts = Int32(HTML_PARSE_RECOVER.rawValue | HTML_PARSE_NOBLANKS.rawValue)
+        if htmlCtxtUseOptions(parserContext, opts) != 0 {
+            os_log(
+                "Parser returned non-zero return code for options",
+                log: .htmlParser,
+                type: .fault,
+                baseURL.absoluteString
+            )
+        }
+
+        if htmlParseDocument(parserContext) != 0,
+            let error = xmlCtxtGetLastError(parserContext)?.pointee,
+            let message = error.message
+        {
+            os_log(
+                "Parser returned non-zero return code for URL %@. Last error message: %@ (%i)",
+                log: .htmlParser,
+                type: error.level == XML_ERR_FATAL ? .error : .debug,
+                baseURL.absoluteString,
+                String(cString: message).trimmingCharacters(in: .newlines),
+                error.code
+            )
+        }
+
+        htmlFreeParserCtxt(parserContext)
+        parserContext = nil
     }
 
     func abortParsing() {
@@ -106,52 +118,58 @@ class HTMLParser {
 
     private func htmlParserHandler() -> htmlSAXHandler {
         var handler = libxml2.htmlSAXHandler()
-        handler.startElement = htmlParserElementStart
+        handler.startElement = { parserPointer, elementNamePointer, attributesArrayPointer in
+            guard let parserPointer, let elementNamePointer else {
+                return
+            }
+
+            let parser = Unmanaged<HTMLParser>.fromOpaque(parserPointer).takeUnretainedValue()
+
+            HTMLParser.didStartElement(
+                parser,
+                elementNamePointer: elementNamePointer,
+                attributesArrayPointer: attributesArrayPointer
+            )
+        }
         return handler
     }
 
-}
-
-private func htmlParserElementStart(
-    _ parser: UnsafeMutableRawPointer?,
-    elementName: UnsafePointer<xmlChar>?,
-    attributesArray: UnsafeMutablePointer<UnsafePointer<xmlChar>?>?
-) {
-    guard let pointer = parser, let cString = elementName else {
-        os_log("Parser returned nil pointers", log: .htmlParser, type: .fault)
-        return
-    }
-
-    // Each element of the C array consists of two NULL terminated C strings.
-    var attributes: [String: String] = [:]
-    var currentAttributeIndex = 0
-    var currentAttributeKey: String?
-
-    while true {
-        guard let attribute = attributesArray?[currentAttributeIndex] else {
-            break
+    private static func didStartElement(
+        _ parser: HTMLParser,
+        elementNamePointer: UnsafePointer<xmlChar>,
+        attributesArrayPointer: UnsafeMutablePointer<UnsafePointer<xmlChar>?>?
+    ) {
+        guard let delegate = parser.delegate else {
+            return
         }
 
-        // If the key is present, parse the attribute value.
-        if let arrayKey = currentAttributeKey {
-            attributes[arrayKey] = String(cString: attribute)
-                .trimmingCharacters(in: .whitespacesAndNewlines)
-            currentAttributeKey = nil
-            // No key is present, therefore parse a key first.
-        } else {
-            currentAttributeKey = String(cString: attribute)
+        let elementName = String(cString: elementNamePointer)
+
+        // The C array consists of one or more key–value pairs of C strings.
+        var attributes = [String: String]()
+        var currentAttributeIndex = 0
+        var currentAttributeKey: String?
+        while let attribute = attributesArrayPointer?[currentAttributeIndex] {
+            if let attributeKey = currentAttributeKey {
+                attributes[attributeKey] = String(cString: attribute)
+                    .trimmingCharacters(in: .whitespacesAndNewlines)
+                currentAttributeKey = nil
+            } else {
+                currentAttributeKey = String(cString: attribute)
+            }
+            currentAttributeIndex += 1
         }
 
-        currentAttributeIndex += 1
+        delegate.parser(parser, didStartElement: elementName, attributes: attributes)
     }
 
-    let parser = Unmanaged<HTMLParser>.fromOpaque(pointer).takeUnretainedValue()
-    let elementName = String(cString: cString)
-    parser.delegate?.parser(parser, didStartElement: elementName, attributes: attributes)
 }
 
-private extension OSLog {
+extension OSLog {
 
-    static let htmlParser = OSLog(subsystem: "--", category: "HTMLParser")
+    fileprivate static let htmlParser = OSLog(
+        subsystem: Bundle.main.bundleIdentifier ?? "--",
+        category: "HTMLParser"
+    )
 
 }
