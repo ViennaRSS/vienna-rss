7bc88bf35bfb9cab92efe658f6411e3a002c52a6
Salih Muhammed <l@larr.net>
2025-12-12 15:19:28 +0200

    add subscription menu item to menubar icon
    Adds "Add Subscription…" menu item to the menubar icon, positioned after
    "Show Main Window" and before the refresh/mark read actions.

--- Vienna/Sources/Application/AppController.m
+++ Vienna/Sources/Application/AppController.m
@@ -1331,6 +1331,10 @@ withReplyEvent:(NSAppleEventDescriptor *)replyEvent
                                  action:@selector(openVienna:)
                           keyEquivalent:@""];
         [statusBarMenu addItem:[NSMenuItem separatorItem]];
+		[statusBarMenu addItemWithTitle:NSLocalizedString(@"Add Subscription…", @"Title of a menu item")
+								 action:@selector(newSubscriptionFromMenuBar:)
+						  keyEquivalent:@""];
+		[statusBarMenu addItem:[NSMenuItem separatorItem]];
 		[statusBarMenu addItemWithTitle:NSLocalizedString(@"Refresh All Subscriptions", @"Title of a menu item")
 								 action:@selector(refreshAllSubscriptions:)
 						  keyEquivalent:@""];
@@ -1920,6 +1924,19 @@ withReplyEvent:(NSAppleEventDescriptor *)replyEvent
  */
 -(IBAction)newSubscription:(id)sender
 {
+    VNASubscribeViewController *viewController = [VNASubscribeViewController instantiateFromStoryboard];
+    [self.mainWindow.contentViewController presentViewControllerAsSheet:viewController];
+}
+
+-(IBAction)newSubscriptionFromMenuBar:(id)sender
+{
+    if (@available(macOS 14, *)) {
+        [NSApp activate];
+    } else {
+        [NSApp activateIgnoringOtherApps:YES];
+    }
+
+    [self showMainWindow:self];
     VNASubscribeViewController *viewController = [VNASubscribeViewController instantiateFromStoryboard];
     [self.mainWindow.contentViewController presentViewControllerAsSheet:viewController];
 }
